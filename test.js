d25c4bf9909cd8cdb0ce857bbd47b72483f0e059
shiptest-rc-me <shiptest.rc.me@gmail.com>
2017-03-24 12:06:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
-setInterval(function () {
-  console.log("Hello world");
-}, 4000);
+var superagent = require("superagent"),
+    chai = require("chai"),
+    expect = chai.expect,
+    should = require("should");
+
+describe("Index", function () {
+  it("renders HTML", function (done) {
+    superagent.get("http://localhost:3000/")
+      .end(function (e, res) {
+        (e === null).should.equal(true);
+        res.text.should.equal("Hey buddy!");
+        done();
+      });
+  });
+});
