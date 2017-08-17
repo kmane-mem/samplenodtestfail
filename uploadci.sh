46f920c3ad2f06769ae62717878b691f49537a33
shiptest-rc-me <shiptest.rc.me@gmail.com>
2017-08-17 19:18:02 +0530

    Update uploadci.sh

--- uploadci.sh
+++ uploadci.sh
@@ -1,3 +1,3 @@
 echo "hello"
-echo "ready to upload in jfrog from ci for verifying in build images"
+echo "ready to upload in jfrog from ci for verifying in build images jfrog packages are installed properly"
 echo "EOF"
